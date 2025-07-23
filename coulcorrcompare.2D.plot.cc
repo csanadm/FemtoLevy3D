3ba33e01f461dbc3e3c175a9956fc62bb30b169a
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2025-07-23 11:21:34 +0200

    Add files via upload

--- coulcorrcompare.2D.plot.cc
+++ coulcorrcompare.2D.plot.cc
@@ -6,14 +6,24 @@
 
 using namespace std;
 
+const double alpha = 1.2;
+const double Ro = 5.7;
+const double Rs = 5.0;
+const double Rl = 6.8;
+const double lambda = 0.8;
+const double betaT = 0.95;
+
 void PrintHists(TCanvas *c1, TH2D* hCfull, TH2D* hCappr, TH2D* hCdiff, const char* tag)
 {
+  hCfull->SetTitle(Form("#alpha=%.1f, R_{o}=%.1f, R_{s}=%.1f, R_{l}=%.1f, #lambda=%.1f, #beta_{T}=%.1f",alpha,Ro,Rs,Rl,lambda,betaT));
   hCfull->Draw("COLZ");
   c1->Print(Form("coulcorrcompare.2D.Cfull.%s.png",tag));
   
+  hCappr->SetTitle(Form("#alpha=%.1f, R_{o}=%.1f, R_{s}=%.1f, R_{l}=%.1f, #lambda=%.1f, #beta_{T}=%.1f",alpha,Ro,Rs,Rl,lambda,betaT));
   hCappr->Draw("COLZ");
   c1->Print(Form("coulcorrcompare.2D.Capprox.%s.png",tag));
   
+  hCdiff->SetTitle(Form("#alpha=%.1f, R_{o}=%.1f, R_{s}=%.1f, R_{l}=%.1f, #lambda=%.1f, #beta_{T}=%.1f",alpha,Ro,Rs,Rl,lambda,betaT));
   hCdiff->Draw("COLZ");
   hCdiff->GetZaxis()->SetRangeUser(2.e-8,8.e-1);
   c1->SetLogz(1);
