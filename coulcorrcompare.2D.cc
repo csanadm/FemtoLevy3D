3aec73c1a68ab06ddc10322733d77fcad81226f7
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2025-07-23 11:12:25 +0200

    Add files via upload

--- coulcorrcompare.2D.cc
+++ coulcorrcompare.2D.cc
@@ -1,71 +1,100 @@
 #include <iostream>
 #include <cmath>
 #include "TH2D.h"
-#include "TCanvas.h"
-#include "TStyle.h"
+#include "TFile.h"
 #include "Levy3D_CoulCalc.h"
 
 using namespace std;
 
 const double hbarcgev = 0.1973269631;
 
+const double alpha = 1.2;
+const double Ro = 5.7;
+const double Rs = 5.0;
+const double Rl = 6.8;
+const double lambda = 0.8;
+const double betaT = 0.95;
+
+Levy3D_CoulCalc* ccc;
+
+void FillHists(const int i1, const int i2, const double qout, const double qside, const double qlong, TH2D* hCfull, TH2D* hCappr, TH2D* hCdiff)
+{
+  double QoPCMS = qout * sqrt(1 - betaT * betaT);
+  double RoPCMS = Ro / sqrt(1 - betaT * betaT);
+  double RPCMS = sqrt((RoPCMS * RoPCMS + Rs * Rs + Rl * Rl) / 3.0);
+  
+  double Cfull = ccc->Full3DCorrFuncValue(alpha, RoPCMS, Rs, Rl, lambda, QoPCMS, qside, qlong);
+  double K2 = ccc->Full3DCoulCorrValue(alpha, RPCMS, RPCMS, RPCMS, 1.0, qout, qside, qlong);
+  double qRq = qout * qout * Ro * Ro + qside * qside * Rs * Rs + qlong * qlong * Rl * Rl;
+  double Capprox = 1 - lambda + lambda * K2 * (1.0 + exp(-pow(qRq/(hbarcgev*hbarcgev), alpha/2.0)));
+  
+  hCfull->SetBinContent(i1, i2, Cfull);
+  hCappr->SetBinContent(i1, i2, Capprox);
+  hCdiff->SetBinContent(i1, i2, abs(Capprox-Cfull));
+  return ;
+}
+
 int main()
 {
-  Levy3D_CoulCalc* ccc = new Levy3D_CoulCalc();
+  TFile* f = new TFile("coulcorrcompare.2D.root", "RECREATE");
+  
+  ccc = new Levy3D_CoulCalc();
   ccc->SetParticleMass(0.13957039);
-
-  double alpha = 1.2;
-  double Ro = 5.7;
-  double Rs = 5.0;
-  double Rl = 6.8;
-  double lambda = 0.8;
-  double betaT = 0.95;
 
   const int Nbins = 100;
   double qmax = 0.10;
 
-  TH2D* hCfull = new TH2D("Cfull2D", "Full C(q_{T},q_{L});q_{T} [GeV];q_{L} [GeV]", Nbins, 0, qmax, Nbins, 0, qmax);
-  TH2D* hCapprox = new TH2D("Capprox2D", "Approximative C(q_{T},q_{L});q_{T} [GeV];q_{L} [GeV]", Nbins, 0, qmax, Nbins, 0, qmax);
-  TH2D* hCdiff = new TH2D("Cdiff2D", "Difference C_{full}-C_{approx};q_{T} [GeV];q_{L} [GeV]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCfullqOqL = new TH2D("Cfull2DqOqL",            "Full C(q_{O},q_{L});q_{O} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCapprqOqL = new TH2D("Cappr2DqOqL",   "Approximative C(q_{O},q_{L});q_{O} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCdiffqOqL = new TH2D("Cdiff2DqOqL", "Difference C_{full}-C_{approx};q_{O} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  
+  TH2D* hCfullqOqS = new TH2D("Cfull2DqOqS",            "Full C(q_{O},q_{S});q_{O} [GeV/c];q_{S} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCapprqOqS = new TH2D("Cappr2DqOqS",   "Approximative C(q_{O},q_{S});q_{O} [GeV/c];q_{S} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCdiffqOqS = new TH2D("Cdiff2DqOqS", "Difference C_{full}-C_{approx};q_{O} [GeV/c];q_{S} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  
+  TH2D* hCfullqSqL = new TH2D("Cfull2DqSqL",            "Full C(q_{S},q_{L});q_{S} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCapprqSqL = new TH2D("Cappr2DqSqL",   "Approximative C(q_{S},q_{L});q_{S} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCdiffqSqL = new TH2D("Cdiff2DqSqL", "Difference C_{full}-C_{approx};q_{S} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  
+  TH2D* hCfullqTqL = new TH2D("Cfull2DqTqL",            "Full C(q_{T},q_{L});q_{T} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCapprqTqL = new TH2D("Cappr2DqTqL",   "Approximative C(q_{T},q_{L});q_{T} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
+  TH2D* hCdiffqTqL = new TH2D("Cdiff2DqTqL", "Difference C_{full}-C_{approx};q_{T} [GeV/c];q_{L} [GeV/c]", Nbins, 0, qmax, Nbins, 0, qmax);
 
-  for (int iT = 1; iT <= Nbins; ++iT)
+  for (int i1 = 1; i1 <= Nbins; ++i1)
   {
-    double qT = hCfull->GetXaxis()->GetBinCenter(iT);
-    for (int iL = 1; iL <= Nbins; ++iL)
+    for (int i2 = 1; i2 <= Nbins; ++i2)
     {
-      double qL = hCfull->GetYaxis()->GetBinCenter(iL);
-
-      // Map to 3D qOut = qT, qSide = 0
-      double qout = qT/sqrt(2.);
-      double qside = qT/sqrt(2.);
-      double qlong = qL;
-
-      double QoPCMS = qout * sqrt(1 - betaT * betaT);
-      double RoPCMS = Ro / sqrt(1 - betaT * betaT);
-      double RPCMS = sqrt((RoPCMS * RoPCMS + Rs * Rs + Rl * Rl) / 3.0);
-
-      double Cfull = ccc->Full3DCorrFuncValue(alpha, RoPCMS, Rs, Rl, lambda, QoPCMS, qside, qlong);
-      double K2 = ccc->Full3DCoulCorrValue(alpha, RPCMS, RPCMS, RPCMS, 1.0, qout, qside, qlong);
-      double qRq = qout * qout * Ro * Ro + qside * qside * Rs * Rs + qlong * qlong * Rl * Rl;
-      double Capprox = 1 - lambda + lambda * K2 * (1.0 + exp(-pow(qRq/(hbarcgev*hbarcgev), alpha/2.0)));
-
-      hCfull->SetBinContent(iT, iL, Cfull);
-      hCapprox->SetBinContent(iT, iL, Capprox);
-      hCdiff->SetBinContent(iT, iL, (Capprox-Cfull)/Cfull);
+      double q1 = (i1-0.5)*(qmax/Nbins);
+      double q2 = (i2-0.5)*(qmax/Nbins);
+      double q3 = q1/sqrt(2.);
+      
+      FillHists(i1, i2, q1,  0, q2, hCfullqOqL, hCapprqOqL, hCdiffqOqL);
+      FillHists(i1, i2, q1, q2,  0, hCfullqOqS, hCapprqOqS, hCdiffqOqS);
+      FillHists(i1, i2, 0,  q1, q2, hCfullqSqL, hCapprqSqL, hCdiffqSqL);
+      FillHists(i1, i2, q3, q3, q2, hCfullqTqL, hCapprqTqL, hCdiffqTqL);
     }
     cerr << ".";
   }
   cerr << endl;
 
-  gStyle->SetOptStat(0);
-  TCanvas* c1 = new TCanvas("c1", "Full", 800, 600);
-  hCfull->Draw("COLZ");
-  c1->Print("coulcorrcompare.2D.Cfull.png");
-  hCapprox->Draw("COLZ");
-  c1->Print("coulcorrcompare.2D.Capprox.png");
-  hCdiff->Draw("COLZ");
-  c1->SetLogz(1);
-  c1->Print("coulcorrcompare.2D.Cdiff.png");
+  hCfullqOqL->Write(); 
+  hCapprqOqL->Write(); 
+  hCdiffqOqL->Write();
+
+  hCfullqOqS->Write();  
+  hCapprqOqS->Write();  
+  hCdiffqOqS->Write(); 
+
+  hCfullqSqL->Write();  
+  hCapprqSqL->Write();  
+  hCdiffqSqL->Write();
+
+  hCfullqTqL->Write();  
+  hCapprqTqL->Write();  
+  hCdiffqTqL->Write();
+  
+  f->Write();
+  f->Close();
 
   return 0;
 }
---
+++ coulcorrcompare.2D.plot.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "TH2D.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TFile.h"
+
+using namespace std;
+
+void PrintHists(TCanvas *c1, TH2D* hCfull, TH2D* hCappr, TH2D* hCdiff, const char* tag)
+{
+  hCfull->Draw("COLZ");
+  c1->Print(Form("coulcorrcompare.2D.Cfull.%s.png",tag));
+  
+  hCappr->Draw("COLZ");
+  c1->Print(Form("coulcorrcompare.2D.Capprox.%s.png",tag));
+  
+  hCdiff->Draw("COLZ");
+  hCdiff->GetZaxis()->SetRangeUser(2.e-8,8.e-1);
+  c1->SetLogz(1);
+  c1->Print(Form("coulcorrcompare.2D.Cdiff.%s.png",tag));
+  return; 
+}
+
+int main()
+{
+  TFile* f = new TFile("coulcorrcompare.2D.root");
+
+  TH2D* hCfullqOqL = (TH2D*)f->Get("Cfull2DqOqL");
+  TH2D* hCapprqOqL = (TH2D*)f->Get("Cappr2DqOqL");
+  TH2D* hCdiffqOqL = (TH2D*)f->Get("Cdiff2DqOqL");
+  
+  TH2D* hCfullqOqS = (TH2D*)f->Get("Cfull2DqOqS");
+  TH2D* hCapprqOqS = (TH2D*)f->Get("Cappr2DqOqS");
+  TH2D* hCdiffqOqS = (TH2D*)f->Get("Cdiff2DqOqS");
+  
+  TH2D* hCfullqSqL = (TH2D*)f->Get("Cfull2DqSqL");
+  TH2D* hCapprqSqL = (TH2D*)f->Get("Cappr2DqSqL");
+  TH2D* hCdiffqSqL = (TH2D*)f->Get("Cdiff2DqSqL");
+  
+  TH2D* hCfullqTqL = (TH2D*)f->Get("Cfull2DqTqL");
+  TH2D* hCapprqTqL = (TH2D*)f->Get("Cappr2DqTqL");
+  TH2D* hCdiffqTqL = (TH2D*)f->Get("Cdiff2DqTqL");
+  
+  gStyle->SetOptStat(0);
+  TCanvas* c1 = new TCanvas("c1", "Full", 800, 600);
+  PrintHists(c1, hCfullqOqL, hCapprqOqL, hCdiffqOqL, "qOqL");
+  PrintHists(c1, hCfullqOqS, hCapprqOqS, hCdiffqOqS, "qOqS");
+  PrintHists(c1, hCfullqSqL, hCapprqSqL, hCdiffqSqL, "qSqL");
+  PrintHists(c1, hCfullqTqL, hCapprqTqL, hCdiffqTqL, "qTqL");
+
+  return 0;
+}
