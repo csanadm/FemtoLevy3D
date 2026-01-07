210637b52c4b0f5fd872c26127b5584d1879f5b7
Mate Csanad <38218165+csanadm@users.noreply.github.com>
2026-01-07 16:53:04 +0100

    Update functions.cpp
    Removed some unnecessary constants and functions

--- functions.cpp
+++ functions.cpp
@@ -1,11 +1,4 @@
 #include "functions.h"
-#define NMIN_SIN 50 // at least this many points in a quarter period of the sine function
-#define NMIN_EXP 40 // at least this many points in one 1/e change of the exponential
-#define LIMIT 30    // if the exponent in the exponential in the integrand is less than minus LIMIT, stop iteration
-#define EPSILON 1e-12 // this is for the terms in the series of the hypergeometric function.
-#define EPSILON_ABC 1e-12 // this is for the terms in the series of the hypergeometric function.
-#define EPSILON_ASYM 1e-6 // this is for the terms in the asymptotic series ofdthe hypergeometric function.
-
 
 // for the Lanczos approximation of the gamma function
 const double lanczos_coeff[9] = {
@@ -50,35 +43,4 @@ double Gamma(const double x)
   double A_g = lanczos_coeff[0] + lanczos_coeff[1] / x + lanczos_coeff[2] / (x + 1.) + lanczos_coeff[3] / (x + 2.) + lanczos_coeff[4] / (x + 3.) + 
                         lanczos_coeff[5] / (x + 4.) + lanczos_coeff[6] / (x + 5.) + lanczos_coeff[7] / (x + 6.) + lanczos_coeff[8] / (x + 7.);
   return sqrt(2. * M_PI) * pow(x + lanczos_g - 0.5, x - 0.5 ) * exp(0.5 - x - lanczos_g) * A_g;
-}
-
-complex<double> Gauss_2F1_series0(const complex<double> a, const complex<double> b, const complex<double> c, const complex<double> z)
-{
-  complex<double> result(-9999., 0);
-  if(abs(z) < 0.9999)
-  {
-    complex<double> n(0., 0.);
-    complex<double> term(1., 0);
-    result = term;
-    while(abs(term) > EPSILON)
-    {
-      term = term * z / (n + 1.) * (a + n) * (b + n) / (c + n);
-      result += term;
-      n += 1. ;
-    }
-  }
-  return result;
-}
-
-complex<double> Gauss_2F1_1mZ_noint(const complex<double> a, const complex<double> b, const complex<double> c, const complex<double> z)
-{
-  complex<double> result(-9999., 0);
-  if(abs(1. - z) < 0.9999)
-  {
-    complex<double> sinabc = sin(M_PI * (c-a-b));
-    if(abs(sinabc) > EPSILON_ABC)
-      result = M_PI / sinabc * Gamma(c) * ( Gauss_2F1_series0(a, b, a+b+1.-c, 1.-z) / Gamma(a+b+1.-c) / Gamma(c-a) / Gamma(c-b)
-                                           - Gauss_2F1_series0(c-a, c-b, c+1.-a-b, 1.-z) / Gamma(a) / Gamma(b) / Gamma(c+1.-a-b) / pow(1.-z, a+b-c));
-  }
-  return result;
 }
